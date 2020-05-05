3258a452fa7efe145d7f276bfeb8d6dfbc6b1897
waheeedkhan00 <64815910+waheeedkhan00@users.noreply.github.com>
2020-05-05 05:57:39 +0500

    Update pathan.py

--- pathan.py
+++ pathan.py
@@ -80,10 +80,10 @@ vuln = "\033[32mVuln"
 os.system("clear")
 print "\033[1;96m ============================================================="
 print  """\033[1;91m=======================================
-\033[1;96mAuthor  \033[1;93m: \033[1;92mRana Aahil
-\033[1;96mInstagram \033[1;93m: \033[1;92mFlowRana
-\033[1;96mFacebook  \033[1;93m: \033[1;92m Aahilrana4072
-\033[1;96mGithub \033[1;93m: \033[1;92mhttps://Github.com/Therana/zero
+\033[1;96mAuthor  \033[1;93m: \033[1;92mWaheed Khan
+\033[1;96mInstagram \033[1;93m: \033[1;92mWaheedkhan00
+\033[1;96mFacebook  \033[1;93m: \033[1;92m Waheedkhan00
+\033[1;96mGithub \033[1;93m: \033[1;92mhttps://Github.com/Waheedkhan00/pathan
 \033[1;91m======================================="""
 print " \x1b[1;93m============================================================="
 
